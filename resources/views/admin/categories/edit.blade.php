fb3513e2425ab7496d497f518ec02d165ca47122
arjungunawan <arjun.gnw09@gmail.com.com>
2023-05-21 22:18:58 +0700

    add, edit, delete user and confirm sweetalert

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -3,23 +3,17 @@
 @section('content')
 
 <div class="post d-flex flex-column-fluid" id="kt_post">
-							<!--begin::Container-->
 	<div id="kt_content_container" class="container-xxl">
         <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10">
             <div class="row g-7 justify-content-center">									
-				<div class="col-xl-8">
-			<form action="{{ url('admin/categories/'.$category->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-                <!--begin::General options-->
-										<div class="card card-flush py-4">
-											<!--end::Card header-->
-											<!--begin::Card body-->
-											<div class="card-body pt-0">
-												<!--begin::Input group-->
-												<div class="mb-10 fv-row">
-													<!--begin::Label-->
-													<label class="required form-label">Kategori Bunga</label>
+				<div class="col-xl-10">
+					<form action="{{ url('admin/categories/'.$category->id) }}" method="POST">
+					@csrf
+					@method('PUT')
+                		<div class="card card-flush py-4">
+							<div class="card-body pt-0">
+								<div class="mb-10 fv-row">
+									<label class="required form-label">Kategori Bunga</label>
 													<!--end::Label-->
 													<!--begin::Input-->
 													<input type="text" name="categoryName" class="form-control mb-2 @error('categoryName') is-invalid @enderror" value="{{ $category->categoryName }}" placeholder="Nama Kategori" />
@@ -32,7 +26,7 @@
                                                 </div>
 												<!--end::Input group-->
                                                 <!--begin::Button-->
-											<a href="{{ url('admin/categories') }}" id="kt_ecommerce_add_product_cancel" class="btn btn-light me-5">Cancel</a>
+											<a href="{{ url('admin/categories') }}" class="btn btn-secondary me-5">Cancel</a>
 											<!--end::Button-->
 											<!--begin::Button-->
 											<button type="submit" class="btn btn-primary">
