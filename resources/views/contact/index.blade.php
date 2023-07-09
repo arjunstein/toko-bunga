810a928f3889b3bdd0295f4d46a8e29a62aafbfd
arjungunawan <arjun.gnw09@gmail.com.com>
2023-07-09 16:08:11 +0700

    test

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -25,7 +25,7 @@
     </div>
     <div class="col-md-10">
         <div class="card">
-            <div class="card-header"><b>Alamat kami</b></div>
+            <div class="card-header"><b>Alamat kami..</b></div>
             <div class="card-body">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.7418981079018!2d106.80351317485422!3d-6.165309093821934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f72b0b69c4d7%3A0x3efb9f1a21a104c!2sOtim%20Florist!5e0!3m2!1sid!2sid!4v1688892358462!5m2!1sid!2sid"
