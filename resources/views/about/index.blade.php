6e2e17890132e1b4911ef03289ae46f2b3ab11f9
arjungunawan <arjun.gnw09@gmail.com.com>
2023-07-09 15:08:57 +0700

    fixing about us page

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -4,13 +4,33 @@
     <div class="col-md-10">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Otim Florist Jakarta</h5>
+                <h5 class="card-title">Selamat datang di Otim Florist Jakarta</h5>
                 <p class="card-text" style="font-size: 16px; text-align:justify;">Dengan pengalaman lebih dari 10 tahun
                     membuat kami dipercaya sebagai produsen karangan bunga khususnya di area JABODETABEK. Kami melayani
                     berbagai keperluan karangan bunga seperti
-                    bunga duka cita, bunga happy wedding, bunga congratulation, bunga standing, bunga meja, hand bouquet
+                    bunga duka cita, bunga happy wedding, bunga congratulation, bunga standing, bunga meja, hand bouquet.
+                    Kami adalah toko bunga yang berkomitmen untuk menyediakan pengalaman berbelanja bunga yang indah dan
+                    memuaskan bagi pelanggan kami. Dengan perpaduan antara keahlian tangan kami yang berbakat dan
+                    bunga-bunga segar berkualitas tinggi, kami berusaha untuk memberikan sentuhan istimewa dalam setiap
+                    rangkaian bunga yang kami buat.
                 </p>
-                <a href="#" class="btn btn-primary">Testimoni</a>
+                <p class="card-text" style="font-size: 16px; text-align:justify;">Otim Florist didirikan dengan tujuan untuk
+                    membawa kebahagiaan dan keindahan melalui bunga-bunga segar yang mempesona. Kami memahami betapa
+                    pentingnya momen-momen istimewa dalam hidup Anda, dan itulah mengapa kami berdedikasi untuk memberikan
+                    produk-produk berkualitas dan pelayanan pelanggan yang luar biasa.</p>
+                <p class="card-text" style="font-size: 16px; text-align:justify;">Keindahan dalam Setiap Rangkaian
+                    Kami percaya bahwa bunga memiliki kekuatan untuk menyampaikan perasaan dan menciptakan atmosfer yang
+                    mengagumkan. Di Otim Florist, kami dengan hati-hati memilih bunga-bunga terbaik untuk menciptakan
+                    rangkaian yang elegan dan memikat. Setiap rangkaian bunga kami adalah hasil dari perpaduan kreativitas,
+                    keahlian tangan, dan cinta terhadap detail.
+                </p>
+                <p class="card-text" style="font-size: 16px; text-align:justify;">Kepuasan pelanggan adalah prioritas utama
+                    kami. Kami berusaha untuk memberikan pengalaman berbelanja yang menyenangkan dan pelayanan yang ramah.
+                    Tim kami yang berdedikasi akan membantu Anda memilih rangkaian bunga yang sesuai dengan preferensi dan
+                    kebutuhan Anda. Kami selalu berusaha untuk memberikan produk-produk berkualitas tinggi dan tepat waktu.
+                </p>
+                <a href="/testimoni" class="btn btn-primary">Lihat Testimoni</a>
+                <a href="/toko" class="btn btn-success">Beli bunga</a>
             </div>
         </div>
     </div>
